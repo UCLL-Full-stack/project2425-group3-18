88d5a7af9fca0daaaa151605a744f9275076ac8a
DaanHoeven <r0985628@ucll.be>
2024-11-03 09:43:20 +0100

    updated swagger for getAllKoten

--- back-end/controller/kot.router.ts
+++ back-end/controller/kot.router.ts
@@ -1,75 +1,77 @@
 /**
  * @swagger
- *   components:
- *    schemas:
- *      User:
- *          type: object
- *          properties:
- *            userName:
- *              type: string
- *              description: The user's name.
- *            email:
- *              type: string
- *              description: The user's email address.
- *            password:
- *              type: string
- *              description: The user's password.
- *            profile:
- *              type: object
- *              nullable: true
- *              description: The profile associated with the user.
- * 
- *      Profile:
- *          type: object
- *          properties:
- *            firstName:
- *              type: string
- *              description: First name of the profile user.
- *            lastName:
- *              type: string
- *              description: Last name of the profile user.
- *            bio:
- *              type: string
- *              description: Bio of the profile user.
- *            role:
- *              type: string
- *              description: Role of the user in the platform.
- *            user:
- *              $ref: '#/components/schemas/User'
- *              description: The user associated with this profile.
- *            posts:
- *              type: array
- *              items:
- *                type: object
- *                properties:
- *                  description:
- *                    type: string
- *                  image:
- *                    type: string
- *              description: List of posts made by the user.
- *            koten:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Kot'
- *              description: List of koten associated with the profile.
- * 
- *      Kot:
- *          type: object
- *          properties:
- *            location:
- *              type: string
- *              description: The location of the kot.
- *            price:
- *              type: number
- *              description: The price of the kot.
- *            surfaceSpace:
- *              type: number
- *              description: The surface space of the kot in square meters.
- *            profiles:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/Profile'
- *              description: List of profiles associated with the kot.
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         userName:
+ *           type: string
+ *           description: The user's name.
+ *         email:
+ *           type: string
+ *           description: The user's email address.
+ *         password:
+ *           type: string
+ *           description: The user's password.
+ *         profile:
+ *           $ref: '#/components/schemas/Profile'
+ *           nullable: true
+ *           description: The profile associated with the user.
+ *
+ *     Profile:
+ *       type: object
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: First name of the profile user.
+ *         lastName:
+ *           type: string
+ *           description: Last name of the profile user.
+ *         bio:
+ *           type: string
+ *           description: Bio of the profile user.
+ *         role:
+ *           type: string
+ *           description: Role of the user in the platform.
+ *         user:
+ *           $ref: '#/components/schemas/User'
+ *           description: The user associated with this profile.
+ *         posts:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: Description of the post.
+ *               image:
+ *                 type: string
+ *                 description: Image URL associated with the post.
+ *           description: List of posts made by the user.
+ *         koten:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Kot'
+ *           description: List of koten associated with the profile.
+ *
+ *     Kot:
+ *       type: object
+ *       properties:
+ *         location:
+ *           type: string
+ *           description: The location of the kot.
+ *         price:
+ *           type: number
+ *           description: The price of the kot.
+ *         surfaceSpace:
+ *           type: number
+ *           description: The surface space of the kot in square meters.
+ *         profiles:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Profile'
+ *           description: List of profiles associated with the kot.
  */
 import express, { NextFunction, Request, Response } from 'express';
 import kotService from "../service/kot.service";
